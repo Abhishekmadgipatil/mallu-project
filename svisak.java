22a3dcba985f556bc3f53da483cfd29df19258b2
Abhishekmadgipatil <125115285+Abhishekmadgipatil@users.noreply.github.com>
2023-04-21 10:26:18 +0530

    Add files via upload

---
+++ svisak.java
@@ -0,0 +1,12 @@
+package abhi;
+class Test{
+	protected int x,y;
+}
+public class svisak {
+	public static void main(String[] args) {
+		Test t=new Test();
+		System.out.println(t.x+""+t.y);
+	}
+	
+	}
+
